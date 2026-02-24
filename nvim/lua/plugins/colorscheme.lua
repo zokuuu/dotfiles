adc10a9f2efba84c2124c4ab2a6604aecb59654d
zokuuu <nikasymov@icloud.com>
2026-02-24 15:05:26 +0300

    gruvbox and delete zed. neovim best text editor for minimalism and focus work

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,9 @@
--- return {
---   "sainnhe/sonokai",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.g.sonokai_transparent_background = "1"
---     vim.g.sonokai_enable_italic = true
---     vim.g.sonokai_style = "andromeda"
---     vim.cmd.colorscheme("sonokai")
---   end,
--- }
-
 return {
-  "folke/tokyonight.nvim",
-  opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
-    },
-  },
+  "morhetz/gruvbox",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_contrast_dark = "hard"
+    vim.cmd("colorscheme gruvbox")
+  end,
 }
--- return {
---   "everblush/everblush.vim",
---   lazy = false,
---   priority = 1000,
---   config = function() end,
--- }
